5c42b386cd5a52ab5baf9e9424aed48100db1eb7
Cause Chung <cuzfrog@gmail.com>
2024-10-07 13:15:33 +0800

    update mount-tmpfs.sh script

--- mount-tmpfs.sh
+++ mount-tmpfs.sh
@@ -3,9 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function tmpfsify() {
   mkdir -p "$DIR/$1"
-  mount -t tmpfs -o size="$2" -o noatime tmpfs "$DIR/$1"
+  sudo mount -t tmpfs -o size="$2" -o noatime tmpfs "$DIR/$1"
 }
 
 tmpfsify "target" 64M
-tmpfsify "an/target" 128M
-tmpfsify "ap/target" 256M
+tmpfsify "annotation/target" 128M
+tmpfsify "processor/target" 256M
+echo "tmpfs mounted"
