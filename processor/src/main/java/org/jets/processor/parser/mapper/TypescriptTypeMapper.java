d50706886d063a984c87944d5d344d96f709ede7
Cause Chung <cuzfrog@gmail.com>
2024-10-11 17:13:39 +0800

    Implementing TypescriptTypeMapper

--- processor/src/main/java/org/jets/processor/parser/mapper/TypescriptTypeMapper.java
+++ processor/src/main/java/org/jets/processor/parser/mapper/TypescriptTypeMapper.java
@@ -3,17 +3,27 @@ package org.jets.processor.parser.mapper;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 
-import lombok.RequiredArgsConstructor;
+import org.jets.processor.GlobalContext;
 import org.jets.processor.domain.TypeSymbol;
 
-@RequiredArgsConstructor(onConstructor_ = @Inject)
 @Singleton
 final class TypescriptTypeMapper implements TypeMapper {
+  private final GlobalContext ctx;
+  private final Elements elemUtils;
+  private final Types typeUtils;
+
+  @Inject
+  TypescriptTypeMapper(GlobalContext ctx) {
+    this.ctx = ctx;
+    this.elemUtils = ctx.getProcessingEnv().getElementUtils();
+    this.typeUtils = ctx.getProcessingEnv().getTypeUtils();
+  }
 
   @Override
   public TypeSymbol map(TypeMirror typeMirror) {
-    
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'map'");
   }
