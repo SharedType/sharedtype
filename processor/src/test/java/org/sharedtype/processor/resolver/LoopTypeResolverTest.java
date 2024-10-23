d3c1506d0f77823e94761e71f714ec339ee0e975
Cause Chung <cuzfrog@gmail.com>
2024-10-23 17:01:56 +0800

    Test LoopTypeResolver

--- processor/src/test/java/org/sharedtype/processor/resolver/LoopTypeResolverTest.java
+++ processor/src/test/java/org/sharedtype/processor/resolver/LoopTypeResolverTest.java
@@ -11,6 +11,7 @@ import org.sharedtype.processor.parser.TypeDefParser;
 import javax.lang.model.element.TypeElement;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -25,19 +26,17 @@ final class LoopTypeResolverTest {
             .qualifiedName("com.github.cuzfrog.Abc")
             .name("Abc")
             .components(List.of(
-                FieldComponentInfo.builder().name("a").type(
+                FieldComponentInfo.builder().name("tuple").type(
                     ConcreteTypeInfo.builder()
-                        .qualifiedName("com.github.cuzfrog.Map")
-                        .simpleName("Map")
+                        .qualifiedName("com.github.cuzfrog.Tuple")
+                        .resolved(false)
                         .typeArgs(List.of(
                             ConcreteTypeInfo.builder()
-                                .qualifiedName("com.github.cuzfrog.Key")
-                                .simpleName("Key")
+                                .qualifiedName("com.github.cuzfrog.A")
                                 .resolved(false)
                                 .build(),
                             new ArrayTypeInfo(ConcreteTypeInfo.builder()
-                                .qualifiedName("com.github.cuzfrog.Abc")
-                                .simpleName("Abc")
+                                .qualifiedName("com.github.cuzfrog.B")
                                 .resolved(false)
                                 .build())
                         ))
@@ -45,11 +44,49 @@ final class LoopTypeResolverTest {
                 ).build()
             ))
             .build();
-        when(typeDefParser.parse(mockElementByName("com.github.cuzfrog.Abc"))).thenReturn(
-            ClassDef.builder().build()
-        );
 
-//        var defs = resolver.resolve(List.of(typeDef));
+        var tupleDef = ClassDef.builder().qualifiedName("com.github.cuzfrog.Tuple").name("Tuple").build();
+        when(typeDefParser.parse(mockElementByName("com.github.cuzfrog.Tuple"))).thenReturn(tupleDef);
+        var aDef = ClassDef.builder().qualifiedName("com.github.cuzfrog.A").name("A").build();
+        when(typeDefParser.parse(mockElementByName("com.github.cuzfrog.A"))).thenReturn(aDef);
+        var bDef = ClassDef.builder().qualifiedName("com.github.cuzfrog.B").name("B").build();
+        when(typeDefParser.parse(mockElementByName("com.github.cuzfrog.B"))).thenReturn(bDef);
+
+        var defs = resolver.resolve(List.of(typeDef));
+        assertThat(defs).hasSize(4);
+        {
+            var b = (ClassDef) defs.get(0);
+            assertThat(b).isSameAs(bDef);
+        }
+        {
+            var a = (ClassDef) defs.get(1);
+            assertThat(a).isSameAs(aDef);
+        }
+        {
+            var tuple = (ClassDef) defs.get(2);
+            assertThat(tuple).isSameAs(tupleDef);
+        }
+        {
+            var abc = (ClassDef) defs.get(3);
+            assertThat(abc).isSameAs(typeDef);
+            FieldComponentInfo field = abc.components().get(0);
+            assertThat(field.resolved()).isTrue();
+            assertThat(field.name()).isEqualTo("tuple");
+            ConcreteTypeInfo fieldType = (ConcreteTypeInfo) field.type();
+            assertThat(fieldType.simpleName()).isEqualTo("Tuple");
+            assertThat(fieldType.typeArgs()).satisfiesExactly(
+                a -> {
+                    assertThat(a.resolved()).isTrue();
+                    assertThat(((ConcreteTypeInfo) a).simpleName()).isEqualTo("A");
+                },
+                bArr -> {
+                    assertThat(bArr.resolved()).isTrue();
+                    ConcreteTypeInfo arrComp = (ConcreteTypeInfo) ((ArrayTypeInfo)bArr).component();
+                    assertThat(arrComp.resolved()).isTrue();
+                    assertThat(arrComp.simpleName()).isEqualTo("B");
+                }
+            );
+        }
     }
 
     private TypeElement mockElementByName(String qualifiedName) {
