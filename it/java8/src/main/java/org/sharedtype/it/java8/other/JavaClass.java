cc8f3b164187801f51d67ea750cce506cdf7b8ab
Cause Chung <cuzfrog@gmail.com>
2024-11-03 21:30:10 +0800

    Error on name conflict for typescript

--- it/java8/src/main/java/org/sharedtype/it/java8/other/JavaClass.java
+++ it/java8/src/main/java/org/sharedtype/it/java8/other/JavaClass.java
@@ -2,6 +2,7 @@ package org.sharedtype.it.java8.other;
 
 import org.sharedtype.annotation.SharedType;
 
-@SharedType
+@SharedType(name = "AnotherJavaClass")
 public class JavaClass {
+    int value;
 }
