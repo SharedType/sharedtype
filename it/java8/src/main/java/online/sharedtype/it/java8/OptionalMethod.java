8de56eb1eca11f8156d109d123c7d5225ecf18e3
Cause Chung <cuzfrog@gmail.com>
2025-02-23 16:54:19 -0500

    Resolve optional types

--- it/java8/src/main/java/online/sharedtype/it/java8/OptionalMethod.java
+++ it/java8/src/main/java/online/sharedtype/it/java8/OptionalMethod.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import online.sharedtype.SharedType;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
@@ -27,5 +28,10 @@ public class OptionalMethod {
     @JsonGetter
     Optional<Set<Optional<String>>> getSetNestedValueOptional() {
         return Optional.of(Collections.singleton(Optional.ofNullable(value)));
+    }
+
+    @JsonGetter
+    Optional<Map<Integer, Optional<String>>> getMapNestedValueOptional() {
+        return Optional.of(Collections.singletonMap(1, Optional.ofNullable(value)));
     }
 }
