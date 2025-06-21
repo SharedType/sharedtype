669b27640fe83341f20cb23a75772587c593e9f1
Cause Chung <cuzfrog@gmail.com>
2025-06-21 12:03:59 -0400

    Get maven compiler

--- maven-plugin/src/main/java/online/sharedtype/maven/SharedTypeGenMojo.java
+++ maven-plugin/src/main/java/online/sharedtype/maven/SharedTypeGenMojo.java
@@ -5,15 +5,31 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
 
 import javax.inject.Inject;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import java.util.ServiceLoader;
 
 @Mojo(name = "gen")
 public final class SharedTypeGenMojo extends AbstractMojo {
+    private static final String COMPILER_ID = "javac";
     @Inject
     private MavenSession session;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        MavenProject project = session.getCurrentProject();
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+    }
+
+    private JavaCompiler getJavaCompiler() {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        if (compiler != null) {
+            return compiler;
+        }
+        throw new IllegalStateException("Java compiler not found.");
     }
 }
