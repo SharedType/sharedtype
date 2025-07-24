ef617fbc86d0a2ffc242335898531ed935585a34
Cause Chung <cuzfrog@gmail.com>
2025-07-23 21:30:37 -0400

    Setup gradle plugin publish

--- build-tool-plugins/gradle-plugin/build.gradle.kts
+++ build-tool-plugins/gradle-plugin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-gradle-plugin`
-    `maven-publish`
+    id("com.gradle.plugin-publish") version "1.2.1"
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 java {
@@ -9,7 +10,6 @@ java {
 }
 
 group = "online.sharedtype"
-version = "0.14.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -25,12 +25,46 @@ dependencies {
 }
 
 gradlePlugin {
-    val greeting by plugins.creating {
-        id = "online.sharedtype.sharedtype-gradle-plugin"
-        implementationClass = "online.sharedtype.gradle.SharedtypeGradlePlugin"
+    website = "https://github.com/SharedType/sharedtype"
+    vcsUrl = "https://github.com/SharedType/sharedtype.git"
+    plugins {
+        create("sharedtype") {
+            id = "online.sharedtype.sharedtype-gradle-plugin"
+            displayName = "SharedType Gradle Plugin"
+            description = "SharedType Gradle Plugin, SharedType is the tool to generate types from Java to target languages."
+            tags = listOf("sharedtype", "java", "annotation processing", "type generation")
+            implementationClass = "online.sharedtype.gradle.SharedtypeGradlePlugin"
+        }
     }
 }
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+mavenPublishing {
+    pom {
+        name.set("SharedType Gradle Plugin")
+        description.set("SharedType Gradle Plugin, SharedType is the tool to generate types from Java to target languages.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/SharedType/sharedtype")
+        licenses {
+            license {
+                name.set("CC BY 4.0")
+                url.set("https://creativecommons.org/licenses/by/4.0/deed.en")
+                distribution.set("https://creativecommons.org/licenses/by/4.0/deed.en")
+            }
+        }
+        developers {
+            developer {
+                id.set("cuzfrog")
+                name.set("Cause Chung")
+                url.set("https://github.com/cuzfrog")
+            }
+        }
+        scm {
+            url.set("https://github.com/SharedType/sharedtype")
+            connection.set("scm:git@github.com:SharedType/sharedtype.git")
+        }
+    }
 }
